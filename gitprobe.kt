b8c58d9c74d143d52767729e661b69ddcdb1cb0a
Pauló Lajos <46568469+PauloLajos@users.noreply.github.com>
2022-11-27 05:37:59 +0100

    Update gitprobe.kt

--- gitprobe.kt
+++ gitprobe.kt
@@ -1,6 +1,11 @@
 git config --global user.name "Lajos Paulo"
 git config --global user.email "paulolajos@mail.com"
 
+git remote add origin https://github.com/PauloLajos/kotlin_apprentice.git
+git branch -M "main"
+git push -u origin "main"
+
 git add .
 git commit -m "My commit message"
-git push -u origin HEAD
+git push origin main
+//git push -u origin HEAD
